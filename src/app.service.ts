bbeefae7ee1367f1b6a11a3d8c108a23709ad9b7
Sachinm2194 <107804052+Sachinm2194@users.noreply.github.com>
2026-01-12 19:24:41 +0530

    welcome Message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World its LUMIAURA!!!';
+    return 'Welcome to LumiAura API! 🚀';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
